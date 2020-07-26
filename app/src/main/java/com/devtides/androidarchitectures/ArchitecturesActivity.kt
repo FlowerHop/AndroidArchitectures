e3ad281d52aff51d668d1522764b674595ee1a1b
FlowerHop <l941166@hotmail.com>
2020-07-26 11:21:25 +0800

    My Kotlin first try
    Change ArchitecturesActivity.java to ArchitecturesActivity.kt.

---
+++ app/src/main/java/com/devtides/androidarchitectures/ArchitecturesActivity.kt
@@ -0,0 +1,27 @@
+package com.devtides.androidarchitectures
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import com.devtides.androidarchitectures.mvc.MVCActivity
+import com.devtides.androidarchitectures.mvp.MVPActivity
+import com.devtides.androidarchitectures.mvvm.MVVMActivity
+
+class ArchitecturesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_architectures)
+    }
+
+    fun onMVC(view: View) {
+        startActivity(MVCActivity.getIntent(this))
+    }
+
+    fun onMVP(view: View) {
+        startActivity(MVPActivity.getIntent(this))
+    }
+
+    fun onMVVM(view: View) {
+        startActivity(MVVMActivity.getIntent(this))
+    }
+}
